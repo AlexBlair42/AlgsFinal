1dd588b0e206d46e1f3562da7bd1d723dcff4d73
saleisha57 <abagnall-smith20@my.whitworth.edu>
2018-12-13 19:17:58 -0800

    Add files via upload

---
+++ Final/src/kruskal (2).py
@@ -0,0 +1,58 @@
+# Referenced https://www.saltycrane.com/blog/2008/01/how-to-find-intersection-and-union-of/ for unions.
+# Referenced https://www.geeksforgeeks.org/detect-cycle-in-a-graph/ for checking if acyclic.
+
+# File Input
+file = open("matrix2.txt")
+graph = [[int(val) for val in line.split()] for line in file]
+vertices = []
+cost = 0
+
+
+# Filling vertices
+for r in range(0, len(graph)):
+	vertices.append(r)
+
+	
+# Set up a list to manage visited nodes.
+visit = [False] * len(vertices)
+
+
+#Checking to see if there are any cycles in the minimum spanning tree.
+def isAcyclic(x):
+	for num in range(len(x)):
+		if visit[num] == False:
+			return True
+	return False
+
+	
+# Get the union of "Answer" and "list of edges from "graph".
+def union(uA, uE):
+	return list(set(uA) | set(uE))
+
+	
+# Selects edges based on their weight and traverses the graph making a minimum spanning tree.
+def kruskal():
+	answer = []
+	encounter = 0
+	k = 0
+	while encounter < (len(vertices)-1):
+		k += 1
+		if isAcyclic(union(answer,graph[k])):
+			answer = union(answer,graph[k])
+			encounter += 1
+	return answer
+
+	
+# Set output to the minimum spanning tree.
+output = kruskal()
+
+
+# Add up the cost of the spanning tree.
+for x in range(0, len(output)):
+	cost += output[x]
+
+
+# Display the minimum spanning tree and the total cost.
+#print(output)
+print(cost)
+print(output)
